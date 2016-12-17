b24aa45078df70712da39c778c6a68011e55813c
Peter Jackson <yooniks@gmail.com>
2016-12-17 12:38:30 -0500

    Topic resource init

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161217173717) do
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "title",      limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title",       limit: 255,   null: false
+    t.string   "subtitle",    limit: 255,   null: false
+    t.text     "body",        limit: 65535, null: false
+    t.integer  "category_id", limit: 4,     null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "topics", ["category_id"], name: "index_topics_on_category_id", using: :btree
+
+  add_foreign_key "topics", "categories"
 end
