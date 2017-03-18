78a7f91cf94a15159c8fad81f8bf8be91ad9afbc
Tsi Lung <tsi.lung@gmail.com>
2017-03-18 16:42:47 -0400

    update destroy

--- app/controllers/event_favorites_controller.rb
+++ app/controllers/event_favorites_controller.rb
@@ -1,19 +1,20 @@
 class EventFavoritesController < ApplicationController
 
   def create
+    debugger
     @event_favorite = current_user.event_favorites.new(event_id: params[:event_id])
 
     if @event_favorite.save
-      render template: 'event_favorite/event_favorites'
+      render partial: 'event_favorites/event_favorite', locals: { event_favorite: @event_favorite}
     else
       render json: @event_favorite.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @event_user_relationship = EventUserRelationship.find_by(event_id: params[:event_id])
+    @event_favorite = EvenFavorite.find_by(event_id: params[:event_id], user_id: current_user)
 
     # TODO: Replace this with deactivate when ActiveStateHelper is fixed
-    @event_user_relationship.update :is_active, false
+    @event_user_relationship.update(is_active: false)
   end
 end
